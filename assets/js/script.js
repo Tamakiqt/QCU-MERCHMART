8a487ab8bab5e5eae63180141dfdf1344ae8b0bf
Adam Nuevo <adamnuevo66@gmail.com>
2024-11-01 12:47:35 -0800

    First Commit

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// Optional: Redirect using JavaScript
+document.getElementById('loginIcon').addEventListener('click', function (event) {
+    event.preventDefault(); 
+    window.location.href = 'login.html'; 
+  });
+  
+  // For Registration
+
+  document.getElementById('signupLink').addEventListener('click', function (event) {
+    event.preventDefault(); // Prevent default link behavior
+    window.location.href = 'register.html'; // Redirect to register page
+  });
+
+
+  // For login
