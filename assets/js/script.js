ba661afb51d2dea7d1b6bec73d4211bc5e8c6d30
Adam Nuevo <adamnuevo66@gmail.com>
2024-11-02 16:36:55 -0800

    NAKAKASTRESS

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ document.getElementById('loginIcon').addEventListener('click', function (event) 
   
   // For Registration
 
-  document.getElementById('signupLink').addEventListener('click', function (event) {
+  document.getElementById('register-url').addEventListener('click', function (event) {
     event.preventDefault(); // Prevent default link behavior
     window.location.href = 'register.html'; // Redirect to register page
   });
