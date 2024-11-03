3764e315a205122f33cebc7e7500150a95d9f53b
Adam Nuevo <adamnuevo66@gmail.com>
2024-11-03 04:30:49 -0800

    Suko na HAHAHH

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,1 @@
-// Optional: Redirect using JavaScript
-document.getElementById('loginIcon').addEventListener('click', function (event) {
-    event.preventDefault(); 
-    window.location.href = 'login.html'; 
-  });
-  
-  // For Registration
 
-  document.getElementById('register-url').addEventListener('click', function (event) {
-    event.preventDefault(); // Prevent default link behavior
-    window.location.href = 'register.html'; // Redirect to register page
-  });
-
-
-  // For login
