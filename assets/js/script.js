856d862101a58cf22bc97a4d715d1b2003235d2c
Adam Nuevo <adamnuevo66@gmail.com>
2024-11-07 23:19:22 -0800

    NAKAKASTRESS!!!!

--- assets/js/script.js
+++ assets/js/script.js
@@ -96,16 +96,16 @@ document.addEventListener("DOMContentLoaded", function() {
 
 const imagePaths = [
     "assets/images/IMG_0052.PNG", // Main image (index 0)
-    "assets/images/IMG_0041.PNG",
-    "assets/images/IMG_0042.PNG",
-    "assets/images/IMG_0043.PNG",
-    "assets/images/IMG_0044.PNG",
-    "assets/images/IMG_0045.PNG",
-    "assets/images/IMG_0046.PNG",
-    "assets/images/IMG_0047.PNG",
-    "assets/images/IMG_0048.PNG",
-    "assets/images/IMG_0049.PNG",
-    "assets/images/IMG_0050.PNG"
+    "assets/images/bcs.PNG",
+    "assets/images/beced.PNG",
+    "assets/images/bis.PNG", 
+    "assets/images/bsa.PNG",
+    "assets/images/bscpe.PNG", 
+    "assets/images/bsece.PNG",
+    "assets/images/bsentrep.PNG", 
+    "assets/images/bsie.PNG", 
+    "assets/images/bsit.PNG", 
+    "assets/images/bsma.PNG"
 ];
 
 let currentIndex = 0;
@@ -127,4 +127,72 @@ function changeSlide(direction) {
     const mainImage = document.getElementById("main-image");
     mainImage.src = imagePaths[currentIndex];
 }
+
+ // favorite icon 
+
+document.addEventListener("DOMContentLoaded", function () {
+    const favoriteBtn = document.getElementById("Favorite");
+    const notification = document.getElementById("favoriteNotification");
+
+    favoriteBtn.addEventListener("click", function () {
+        // Toggle the 'favorited' class on the button
+        favoriteBtn.classList.toggle("favorited");
+
+        // Display the notification
+        notification.classList.add("show");
+
+        // Hide the notification after 2 seconds
+        setTimeout(function () {
+            notification.classList.remove("show");
+        }, 3000);
+    });
+});
+
+//For course choices
+
+const courseSelect = document.getElementById('courseSelect');
+const mainImage = document.getElementById('main-image');
+
+courseSelect.addEventListener('change', function() {
+    const selectedCourse = courseSelect.value;
+    let newImageSrc = '';
+
+    switch (selectedCourse) {
+        case 'bcs':
+            newImageSrc = 'assets/images/bcs.PNG';
+            break;
+        case 'beced':
+            newImageSrc = 'assets/images/beced.PNG';
+            break;
+        case 'bis':
+            newImageSrc = 'assets/images/bis.PNG';
+            break;
+        case 'bsa':
+            newImageSrc = 'assets/images/bsa.PNG';
+            break;
+        case 'bscpe':
+            newImageSrc = 'assets/images/bscpe.PNG';
+            break;
+        case 'bsece':
+            newImageSrc = 'assets/images/bsece.PNG';
+            break;
+        case 'bsentrep':
+            newImageSrc = 'assets/images/bsentrep.PNG';
+            break;
+        case 'bsie':
+            newImageSrc = 'assets/images/bsie.PNG';
+            break;
+        case 'bsit':
+            newImageSrc = 'assets/images/bsit.PNG';
+            break;
+        case 'bsma':
+            newImageSrc = 'assets/images/bsma.PNG';
+            break;
+        default:
+            newImageSrc = 'assets/images/IMG_0052.PNG'; // Default image
+    }
+
+    mainImage.src = newImageSrc;
+});
+
 
